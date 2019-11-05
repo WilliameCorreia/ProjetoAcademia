2b5875f4323065cc1f587e03015f958d826b9752
williame <williame_lima@hotmail.com>
2019-11-05 11:07:09 -0300

    primeiro commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
+import {MatButtonModule} from '@angular/material/button';
+
+import { AppRoutingModule } from './app.routing';
+import { ComponentsModule } from './components/components.module';
+import { AppComponent } from './app.component';
+
+import {
+  AgmCoreModule
+} from '@agm/core';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import {MatInputModule} from '@angular/material/input';
+import { AuthComponent } from './auth/auth.component';
+
+@NgModule({
+  imports: [
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    ComponentsModule,
+    RouterModule,
+    AppRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'YOUR_GOOGLE_MAPS_API_KEY'
+    }),
+    MatInputModule,
+    MatButtonModule
+  ],
+  declarations: [
+    AppComponent,
+    AdminLayoutComponent,
+    AuthComponent
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
