914564e7cdf314ca38cf111cfebc6d2dce7cdd13
williame <williame_lima@hotmail.com>
2019-11-11 15:14:45 -0300

    validação firebase

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'app/auth/auth.service';
-import { User } from 'app/auth/user';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,23 +8,43 @@ import { User } from 'app/auth/user';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  user: User = {
-    userName: "",
-    password: ""
-  };
+  
+  form: FormGroup;
+  private formSubmitAttempt: boolean;
+  MessageFireBase: String = ""; 
 
   constructor(
+    private fb: FormBuilder,
     private authservice: AuthService
   ) { }
 
   ngOnInit() {
-    
+    this.form = this.fb.group({
+      userName: ['', Validators.required],
+      password: ['', Validators.required]
+    });
   }
 
-  onLogout(){
-    console.log(this.user);
-    this.authservice.login(this.user);
+  onSubmit(){
+    if(this.form.valid){
+      this.authservice.login(this.form.value);
+    }
+    this.formSubmitAttempt = true;
+    this.authservice.IsValidation.subscribe((message)=>{
+      this.MessageFireBase = message;
+    }); 
   }
 
+  isFieldInvalid(field: string) {
+    return (
+      (!this.form.get(field).valid && this.form.get(field).touched) ||
+      (this.form.get(field).untouched && this.formSubmitAttempt)
+    );
+  }
+
+  isInvalid(){
+    return (
+      (this.MessageFireBase != "")
+    );
+  }
 }
